118d86b6399c7ba0f35e49338be84b95d6c188cd
Htoo Aung Lynn <htooaunglynn6@gmail.com>
2023-01-19 11:48:59 +0630

    add

--- Blog/partials/header.php
+++ Blog/partials/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Title</title>
 
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="./partials/css/style.css">
 
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -15,7 +15,7 @@
     <header>
         <nav class="navbar navbar-expand-lg bg-danger px-5">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Online Shop</a>
+            <a class="navbar-brand" href="./index.php">Online Shop</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
